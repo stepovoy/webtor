7d5cd1749854fd63e606b3bedfdc18050dbe1a56
Artur Stepovyi <astepovy@e3r1p5.unit.ua>
2017-08-25 17:30:53 +0300

    fixed some errors

--- index.js
+++ index.js
@@ -10,27 +10,20 @@ app.get('/', function (req, res) {
   res.render('index.ejs')
 })
 
-app.post('/', function (req, res) {
-	if (WebTorrent.WEBRTC_SUPPORT) {
-  // WebRTC is supported
-  console.log('browser pashet');
-} else {
-  // Use a fallback
-  console.log('vse, browser ne pashet, rasxodims\'ya')
-}
-	var client = new WebTorrent()
-	var magnetURI = 'magnet:?xt=urn:btih:a6cd140e1c329190e634577011a47ec9d1b1e019&dn=Just_A_Movie&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopentor.org%3A2710&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.blackunicorn.xyz%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969'
+// app.post('/', function (req, res) {
+// 	var client = new WebTorrent()
+// 	var magnetURI = 'magnet:?xt=urn:btih:a6cd140e1c329190e634577011a47ec9d1b1e019&dn=Just_A_Movie&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopentor.org%3A2710&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.blackunicorn.xyz%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969'
 	 
-	client.add(magnetURI, function (torrent) {
-	  // Got torrent metadata! 
-	  console.log('Client is downloading:', torrent.infoHash)
+// 	client.add(magnetURI, function (torrent) {
+// 	  // Got torrent metadata! 
+// 	  console.log('Client is downloading:', torrent.infoHash)
 	 
-	  torrent.files.forEach(function (file) {
-	    // Display the file by appending it to the DOM. Supports video, audio, images, and 
-	    // more. Specify a container element (CSS selector or reference to DOM node). 
-	    file.appendTo('body')
-	  })
-	})
-})
+// 	  torrent.files.forEach(function (file) {
+// 	    // Display the file by appending it to the DOM. Supports video, audio, images, and 
+// 	    // more. Specify a container element (CSS selector or reference to DOM node). 
+// 	    file.appendTo('body')
+// 	  })
+// 	})
+// })
 
 app.listen(3000)
